e630609c5c17c370919dfa74897081c2663715fc
tu1026 <s31302@gmail.com>
2021-07-04 02:07:02 -0700

    added some initial models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Coop
 
+admin.site.register(Coop)
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Coop(models.Model):
+    url = models.CharField(max_length=100)
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.url
