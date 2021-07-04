39b0c7eab07f560cf658c22447ea1927932c9396
tu1026 <s31302@gmail.com>
2021-07-03 19:20:07 -0700

    restart project, storing secret key properly

--- djangoWebsite/settings.py
+++ djangoWebsite/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mdj%h)8^kx@m_t&w_^t7$hh=&e@bq$2u91+3q&et6exm48nn0%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
