b3d23b4c0db9bafe17d0969feb5e22d65be504d3
tu1026 <s31302@gmail.com>
2021-07-04 00:11:48 -0700

    adding new app main, routing urls playing aorund with html

--- djangoWebsite/settings.py
+++ djangoWebsite/settings.py
@@ -11,20 +11,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+SECRET_KEY= os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
