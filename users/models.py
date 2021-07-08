b0402d5d6ed612931add7c6a0d85f7e6b42a2f6d
tu1026 <s31302@gmail.com>
2021-07-08 00:19:06 -0700

    adding random dog pics for profiles

--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
+from django.conf import settings
+import random
+random_dogs = os.listdir(settings.MEDIA_ROOT)
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	discord_id=models.CharField(verbose_name="discord_id", max_length=50)
-	image = models.ImageField(default='default.jpg', upload_to="profile_pics")
+	image = models.ImageField(default=random.choice(random_dogs), upload_to="profile_pics")
 
 	def __str__(self) -> str:
 		return f'{self.user.username} Profile'
---
+++ users/test.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+import os
+from django.conf import settings
+
+os.listdir(os.path.join(settings.BASEDIR, "profile_pics"))
