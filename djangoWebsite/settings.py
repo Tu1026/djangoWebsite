4f837bbcbb2a4f941057c0e63e890e39e3b5cc00
tu1026 <s31302@gmail.com>
2021-07-08 23:16:26 -0700

    adding settings for sql for production

--- djangoWebsite/settings.py
+++ djangoWebsite/settings.py
@@ -83,13 +83,24 @@ WSGI_APPLICATION = 'djangoWebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoWebsite',
+        'USER': 'djangoWebsiteUser',
+        'PASSWORD': os.getenv("SQLpassword"),
+        'HOST': 'localhost',
+        'PORT': '',
+        }   
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
