91dbc998f94ba36a68a376a0786d7a157f37877f
tu1026 <s31302@gmail.com>
2021-07-08 17:07:00 -0700

    fixing the debug check

--- djangoWebsite/settings.py
+++ djangoWebsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY= os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG"))
+DEBUG = os.getenv("DEBUG") == "True"
 
 if DEBUG:
     ALLOWED_HOSTS = [ ]
