b9acd5cff9ac991b3ba74e7413cd1898583bdccf
tu1026 <s31302@gmail.com>
2021-07-14 16:31:01 -0700

    updating the main page html as well as adding a tutorial

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='main-home')
+	path('', views.home, name='main-home'),
+	path('tutorial/', views.tutorial, name = "tutorial")
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'main/home.html')
+
+def tutorial(request):
+    return render(request, 'main/tutorial.html')
