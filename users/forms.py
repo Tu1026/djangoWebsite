7b32f653f779b6c217c2fb8ced7f1852eb804191
tu1026 <s31302@gmail.com>
2021-07-14 19:13:32 -0700

    adding checks for profiles as well

--- users/forms.py
+++ users/forms.py
@@ -41,8 +41,26 @@ class ProfileUpdate(forms.ModelForm):
 	class Meta:
 		model= Profile
 		fields = ['discord_id']
+  	
+	def clean_discord_id(self):
+		cleaned_data = self.clean()
+		discord_name = cleaned_data.get('discord_id')
+		name_list = discord_name.strip().split("#")
+		if len(name_list) != 2:
+			raise ValidationError("Please enter the format name#1234")
+		print(name_list)
+		state = False
+		id = 0
+		with ProcessPoolExecutor(max_workers=1) as executor:
+			future = executor.submit(check_valid_name, name_list[0], name_list[1])
+			state, id = future.result()
+			print(f'future result {state} {id}')
+		if not state:  # You create this function
+			raise ValidationError("Your discord name is not in the channel, have you joined the channel yet?")
+		return id
         
 class UserUpdate(forms.ModelForm):
     class Meta:
         model= User
         fields = ['username', 'email']
+    
