f7df22de7bae71ab4300e1bf3d2ccca3c4a5db80
Wilson Tu <s31302@gmail.com>
2022-01-22 00:39:36 -0800

    Make URL's longer to accomodate

--- gym/models.py
+++ gym/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Gym(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	url = models.URLField(verbose_name="URL of the desired 'section' of the the gym session", max_length=200)
+	url = models.URLField(verbose_name="URL of the desired 'section' of the the gym session", max_length=300)
 	name = models.CharField(verbose_name="What is the the time of the session? (So you can know)", max_length=10)
 
 	def __str__(self) -> str:
